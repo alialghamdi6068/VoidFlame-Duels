35045b3ea4c9b92dc421bf1a391056b2c319cc15
alialghamdi6068 <alghamdiali6068@gmail.com>
2026-09-25 17:28:35 +0300

    Initialize VoidFlame-Duels: build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    java
+    id("com.gradleup.shadow") version "9.0.0"
+}
+
+group = "net.voidflame"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:26.2.build.+")
+    compileOnly("net.voidflame:VoidFlame-Core:1.0.0")
+    testImplementation(platform("org.junit:junit-bom:6.0.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(25))
+    withSourcesJar()
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+    options.release.set(25)
+}
+
+tasks.test { useJUnitPlatform() }
+tasks.jar { archiveBaseName.set("VoidFlame-Duels") }
