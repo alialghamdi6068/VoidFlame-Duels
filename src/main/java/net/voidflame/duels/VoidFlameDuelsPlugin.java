e7bed2dd17ca11ee2d09af0d8261f5374245815b
alialghamdi6068 <alghamdiali6068@gmail.com>
2026-09-25 18:22:29 +0300

    Integrate party and kit editor commands

--- src/main/java/net/voidflame/duels/VoidFlameDuelsPlugin.java
+++ src/main/java/net/voidflame/duels/VoidFlameDuelsPlugin.java
@@ -15,6 +15,8 @@ public final class VoidFlameDuelsPlugin extends JavaPlugin {
     private DuelMenu menu;
     private ScoreboardManager scoreboardManager;
     private SpectatorManager spectatorManager;
+    private PartyManager partyManager;
+    private KitEditorManager kitEditorManager;
 
     @Override
     public void onEnable() {
@@ -36,25 +38,38 @@ public final class VoidFlameDuelsPlugin extends JavaPlugin {
         menu = new DuelMenu(this);
         scoreboardManager = new ScoreboardManager(this);
         spectatorManager = new SpectatorManager(this);
+        partyManager = new PartyManager(this);
+        kitEditorManager = new KitEditorManager(this);
 
         coreServices.register(QueueManager.class, queueManager);
         coreServices.register(MatchManager.class, matchManager);
         coreServices.register(ArenaManager.class, arenaManager);
         coreServices.register(KitManager.class, kitManager);
+        coreServices.register(PartyManager.class, partyManager);
+        coreServices.register(KitEditorManager.class, kitEditorManager);
 
         getServer().getPluginManager().registerEvents(queueManager, this);
         getServer().getPluginManager().registerEvents(matchManager, this);
         getServer().getPluginManager().registerEvents(menu, this);
         getServer().getPluginManager().registerEvents(spectatorManager, this);
+        getServer().getPluginManager().registerEvents(partyManager, this);
+        getServer().getPluginManager().registerEvents(kitEditorManager, this);
+
         getServer().getScheduler().runTaskTimer(this, scoreboardManager::updateAll, 20L, 20L);
+        getServer().getScheduler().runTaskTimer(this, partyManager::expireInvites, 20L, 20L);
         getServer().getScheduler().runTask(this, scoreboardManager::updateAll);
 
-        DuelCommand command = new DuelCommand(this);
-        register("duel", command);
-        register("queue", command);
-        register("rematch", command);
-        register("rejoin", command);
-        register("duels", command);
+        DuelCommand duelCommand = new DuelCommand(this);
+        register("duel", duelCommand);
+        register("queue", duelCommand);
+        register("rematch", duelCommand);
+        register("rejoin", duelCommand);
+        register("duels", duelCommand);
+        register("spectate", duelCommand);
+        register("kiteditor", duelCommand);
+
+        PartyCommand partyCommand = new PartyCommand(this);
+        registerParty("party", partyCommand);
 
         getLogger().info("VoidFlame-Duels enabled with 7 ladders.");
     }
@@ -67,18 +82,30 @@ public final class VoidFlameDuelsPlugin extends JavaPlugin {
         }
     }
 
+    private void registerParty(String name, PartyCommand executor) {
+        PluginCommand command = getCommand(name);
+        if (command != null) {
+            command.setExecutor(executor);
+            command.setTabCompleter(executor);
+        }
+    }
+
     @Override
     public void onDisable() {
+        if (kitEditorManager != null) kitEditorManager.shutdown();
         if (matchManager != null) matchManager.shutdown();
         if (spectatorManager != null) spectatorManager.shutdown();
         if (requests != null) requests.clear();
         if (rematches != null) rematches.clear();
+        if (partyManager != null) partyManager.shutdown();
         if (queueManager != null) queueManager.shutdown();
         if (coreServices != null) {
             coreServices.unregister(QueueManager.class);
             coreServices.unregister(MatchManager.class);
             coreServices.unregister(ArenaManager.class);
             coreServices.unregister(KitManager.class);
+            coreServices.unregister(PartyManager.class);
+            coreServices.unregister(KitEditorManager.class);
         }
     }
 
@@ -97,4 +124,6 @@ public final class VoidFlameDuelsPlugin extends JavaPlugin {
     public DuelMenu menu() { return menu; }
     public ScoreboardManager scoreboardManager() { return scoreboardManager; }
     public SpectatorManager spectatorManager() { return spectatorManager; }
+    public PartyManager partyManager() { return partyManager; }
+    public KitEditorManager kitEditorManager() { return kitEditorManager; }
 }
