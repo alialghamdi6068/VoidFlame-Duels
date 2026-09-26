39362933939f001236df242edf09401e1c4beee9
alialghamdi6068 <alghamdiali6068@gmail.com>
2026-09-26 19:44:52 +0300

    Fix combat command event import

--- src/main/java/net/voidflame/duels/MatchManager.java
+++ src/main/java/net/voidflame/duels/MatchManager.java
@@ -9,6 +9,7 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
